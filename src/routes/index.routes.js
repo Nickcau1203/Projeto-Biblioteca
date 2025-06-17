1bb0a2a613f83329f2af141dcb4f8f3043fa14ed
Nickcau1203 <158166129+Nickcau1203@users.noreply.github.com>
2025-06-17 09:12:24 -0300

    feat: quase la

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 // Importar todas as rotas
+import booksRouter from "./books.routes.js";
 import authorsRouter from "./authors.routes.js";
 
 const router = express.Router();
 
 // Rotas públicas
+router.use("/books", booksRouter);
 router.use("/authors", authorsRouter);
 
 // Rotas protegidas
