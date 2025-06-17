15f7cf72641d800b4f1af54a209a7bdd506788b9
Nickcau1203 <158166129+Nickcau1203@users.noreply.github.com>
2025-06-17 08:30:44 -0300

    feat: create model author

--- prisma/migrations/20250617112943_create_data_base/migration.sql
+++ prisma/migrations/20250617112943_create_data_base/migration.sql
@@ -7,3 +7,13 @@ CREATE TABLE "books" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- CreateTable
+CREATE TABLE "authors" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "books" TEXT NOT NULL,
+    "birthdate" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
